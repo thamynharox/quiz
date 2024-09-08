c339f5d7bf7d5ab3c28a20a7732757774b4a8cfb
Thamires Marques Pereira <thamynharox@gmail.com>
2024-09-08 20:26:43 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+export default [
+    {
+      question: "Qual das opções são escolas da Literatura Portuguesa?",
+      answers: [
+        { option: "Trovadorismo e Classicismo", correct: true },
+        { option: "Modernismo e Pós-modernismo", correct: false },
+        { option: "Naturalismo e Parnasianismo", correct: false },
+      ],
+    },
+    {
+      question: "Característica do Classicismo?",
+      answers: [
+        { option: "Poesia lírica e musical", correct: false },
+        { option: "Valorização da cultura greco-romana", correct: true },
+       
+      ],
+    },
+    {
+      question: "Contexto histórico do Trovadorismo?",
+      answers: [
+        { option: "Renascimento", correct: false },
+        { option: "Belle Époque", correct: false },
+        { option: "Idade Média", correct: true },
+      ],
+    },
+  {
+      question: "Agora com a ajuda de Nosso Senhor nos ocupamos em a doutrina destes índios e em rogar ao Senhor que abra a porta para a conversão de muitas nações de que temos novas e em que parece se fará muito fruto por não haver entre elas costume de comer carne humana.",
+      answers: [
+        { option: "Classicismo - Francisco Sá de Miranda", correct: false },
+        { option: "Barroco - Gregório de Matos", correct: false },
+        { option: "Quinhentismo - José de Anchieta", correct: true },
+      ],
+    },
+  {
+      question: "Tiveram longamente na cidade, Sem vender-se, a fazenda os dous feitores, Que os Infiéis, por manha e falsidade, Fazem que não lha comprem mercadores;",
+      answers: [
+        { option: "Trovadorismo - Paio Soares de Taveirós", correct: false },
+        { option: "Barroco - Gregório de Matos", correct: false },
+        { option: "Classicismo - Luís de Camões", correct: true },
+      ],
+    },
+  {
+      question: 
+      "Ai dona fea! Fostes-vos queixar Porque vos nunca louv’ en meu trobar Mais ora quero fazer un cantar En que vos loarei toda via E vedes como vos quero loar: Dona fea, velha e sandia!",
+      answers: [
+        { option: "Trovadorismo - João Garcia de Guilhade", correct: true },
+        { option: "Quinhentismo - Pero Vaz de Caminha", correct: false },
+        { option: "Classicismo - Francisco Sá de Miranda", correct: false },
+      ],
+    },
+    {
+      question: "O amor é finalmente um embaraço de pernas, uma união de barrigas,um breve tremor de artérias, Uma confusão de bocas, uma batalha de veias, um reboliço de ancas, quem diz outra coisa é besta?",
+      answers: [
+        { option: "Quinhentismo", correct: false },
+        { option: "Barroco", correct: true },
+        { option: "Trovadorismo", correct: false },
+      ],
+    },
+  ];
+  
